9d391801f825e3b55777fa0d4e7bfa3e5cc53417
Miriam Lau <meiyen.lau@gmail.com>
2017-11-10 22:00:14 -0800

    Create mage table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171111053540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "type", null: false
+    t.integer "cost", null: false
+    t.string "img_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mages", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "img_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
