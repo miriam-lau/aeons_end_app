db852f9a87a976a4d54fc8f6e460409a4740b252
jamesleejl <33561273+jamesleejl@users.noreply.github.com>
2017-11-14 10:10:09 -0800

    Adds total wins and total games played for each card, mage, nemesis, (#23)
    and player to the JSON.

--- app/models/mage.rb
+++ app/models/mage.rb
@@ -2,4 +2,12 @@ class Mage < ApplicationRecord
   has_many :games_mages
   has_many :games, through: :games_mages
   has_many :players, through: :games_mages
+
+  def total_wins
+    games.where(won: true).count
+  end
+
+  def total_games
+    games.count
+  end
 end
